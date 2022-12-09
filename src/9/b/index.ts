be342f730fcc1d61c9f83d312e0561b676e2eb12
Gerjan <gerjan.dijkhuis@cocreate.nl>
2022-12-09 09:28:12 +0100

    console log

--- src/9/b/index.ts
+++ src/9/b/index.ts
@@ -19,7 +19,6 @@ export default async function () {
   visited.add([...bodyPositions[bodyPositions.length - 1]].toString());
 
   input.forEach(([direction, steps]) => {
-    console.log(direction, steps);
     for (let i = 0; i < +steps; i++) {
       switch (direction) {
         case 'L':
@@ -43,20 +42,20 @@ export default async function () {
         const distance = Math.abs(prev[0] - bodyPosition[0]) + Math.abs(prev[1] - bodyPosition[1]);
         const sameRowOrColumn = prev[0] === bodyPosition[0] || prev[1] === bodyPosition[1];
 
-        let directionsTail: string[] = [];
+        let directionsPart: string[] = [];
 
         if (sameRowOrColumn && distance === 2) {
           if (prev[0] !== bodyPosition[0]) {
-            directionsTail = [prev[0] > bodyPosition[0] ? 'R' : 'L'];
+            directionsPart = [prev[0] > bodyPosition[0] ? 'R' : 'L'];
           } else {
-            directionsTail = [prev[1] > bodyPosition[1] ? 'U' : 'D'];
+            directionsPart = [prev[1] > bodyPosition[1] ? 'U' : 'D'];
           }
         } else if (distance > 2) {
-          directionsTail.push(prev[0] > bodyPosition[0] ? 'R' : 'L');
-          directionsTail.push(prev[1] > bodyPosition[1] ? 'U' : 'D');
+          directionsPart.push(prev[0] > bodyPosition[0] ? 'R' : 'L');
+          directionsPart.push(prev[1] > bodyPosition[1] ? 'U' : 'D');
         }
 
-        directionsTail.forEach(tailDirection => {
+        directionsPart.forEach(tailDirection => {
           switch (tailDirection) {
             case 'L':
               bodyPosition[0]--;
